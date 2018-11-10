45d1288a01e67d3e2083fda64c6e7a0927456948
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2018-11-10 15:10:07 -0800

    changed to dog app

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+//array of current profiles
+var friendsArr = [
+    {
+        name: "dog",
+        photo: "../assets/images/dog.jpg",
+        scores: [1, 2, 1, 2, 1, 2, 1, 2, 1]
+    },
+    {
+        name: "dog2",
+        photo: "http://test.com",
+        scores: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        name: "dog3",
+        photo: "http://test.com",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "dog4",
+        photo: "http://test.com",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "dog5",
+        photo: "../assets/images/dog.jpg",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3]
+    }
+
+];
